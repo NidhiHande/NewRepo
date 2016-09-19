c2677506bc1201f149337079182d7a35c7f99721
Nidhi Hande <Nidhi Hande>
2016-09-20 00:38:20 +0530

    Signed-off-by: Nidhi Hande <Nidhi Hande> gui is commited.

--- A.java
+++ A.java
@@ -4,5 +4,6 @@ public static void main(String args[])
 	System.out.prinln("hi");
 	System.out.prinln("a.java is added to server");
 	System.out.prinln("a new line is added to local repository");
+	System.out.prinln("branch is created");
 }
 }
