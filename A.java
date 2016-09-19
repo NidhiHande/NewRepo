4cb4251b08f71c88001357a2f911aaa8e6350666
NidhiHande <nidhihande95@gmail.com>
2016-09-20 00:19:28 +0530

    Update A.java
    a new line is added to local repository

--- A.java
+++ A.java
@@ -3,7 +3,6 @@ public static void main(String args[])
 {
 	System.out.prinln("hi");
 	System.out.prinln("a.java is added to server");
-	
-	
+	System.out.prinln("a new line is added to local repository");
 }
 }
